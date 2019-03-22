7b2d4404eab6612caf51ca1c05c0c503a4322cf9
Preet Shihn <preetshihn@gmail.com>
2019-03-22 12:11:38 -0700

    initial code

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const byproxy = require('byproxy');
+const app = express();
+const port = 3000;
+
+class Calculator {
+  constructor() {
+    this.opCount = 0;
+  }
+
+  add(a, b) {
+    this.opCount++;
+    return a + b;
+  }
+
+  subtract(a, b) {
+    this.opCount++;
+    return a - b;
+  }
+}
+
+app.use(bodyParser.json())
+app.use('/', express.static('public'));
+
+byproxy.serve(app, '/calculator', new Calculator());
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
